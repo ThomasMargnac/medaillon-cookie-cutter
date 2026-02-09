acfc8331dac7f36da9d8698d42ba7b001b74f22e
Thomas Margnac <78037237+ThomasMargnac@users.noreply.github.com>
2026-02-09 22:09:19 +0100

    feat(utils): adding incremental_load method (#13)

--- {{ cookiecutter.project_slug }}/src/{{ cookiecutter.project_slug }}/utils/commons.py
+++ {{ cookiecutter.project_slug }}/src/{{ cookiecutter.project_slug }}/utils/commons.py
@@ -1,5 +1,6 @@
 import polars as pl
 from datetime import datetime
+from deltalake import DeltaTable, write_deltalake
 
 
 def validate_columns(
@@ -84,3 +85,30 @@ def add_timestamp_column(
     df: pl.DataFrame, column_name: str = "ingestion_timestamp"
 ) -> pl.DataFrame:
     return df.with_columns(pl.lit(datetime.now()).alias(column_name))
+
+
+def incremental_load(
+    df: pl.DataFrame,
+    table_uri: str,
+    predicate: str,
+    storage_options: dict[str, str] | None = None,
+) -> None:
+    if DeltaTable.is_deltatable(table_uri, storage_options=storage_options):
+        dt = DeltaTable(table_uri, storage_options=storage_options)
+        (
+            dt.merge(
+                source=df.to_arrow(),
+                predicate=predicate,
+                source_alias="source",
+                target_alias="target",
+            )
+            .when_matched_update_all()
+            .when_not_matched_insert_all()
+            .execute()
+        )
+    else:
+        write_deltalake(
+            table_or_uri=table_uri,
+            data=df.to_arrow(),
+            storage_options=storage_options,
+        )
